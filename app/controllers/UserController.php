e22cb20a25e05c87657565351d81a8841139901a
Nicholas Mejia <nicknmejia@gmail.com>
2014-09-27 14:38:01 -0700

    Documented functions and added code for 3 in UserController.  Please come back and populate me!

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -3,24 +3,34 @@
 class UserController extends \BaseController {
 
 	/**
-	 * Display a listing of the resource.
+	 * Display all users.  ADMIN ONLY.
 	 *
 	 * @return Response
 	 */
 	public function index()
 	{
-		//
+		// 1. Add key that checks if user has admin privelages
+
+		// 2. if(admin_is_set) Return list of users
+		return View::make('admins.edit_users');
+
+		// 2. else reroute user to dashboard
 	}
 
 
 	/**
-	 * Show the form for creating a new resource.
+	 * Show the form for creating a new user.
 	 *
 	 * @return Response
 	 */
 	public function create()
 	{
-		//
+		// 1. Add key that checks if user has admin privelages
+
+		// 2. if(admin_is_set) Return form for new user
+		return View::make('admin.new_user');
+
+		// 2. else reroute user to dashboard
 	}
 
 
@@ -31,31 +41,39 @@ class UserController extends \BaseController {
 	 */
 	public function store()
 	{
-		//
+		// mySQL for storing new user here
+		// Try using an eloquent model for this.
 	}
 
 
 	/**
-	 * Display the specified resource.
+	 * Display the specified user.
 	 *
 	 * @param  int  $id
 	 * @return Response
 	 */
 	public function show($id)
 	{
-		//
+		// Show users profile before edit
+		// You will need to create a new view for this
 	}
 
 
 	/**
-	 * Show the form for editing the specified resource.
+	 * Show the form for editing the specified user.
 	 *
 	 * @param  int  $id
 	 * @return Response
 	 */
 	public function edit($id)
 	{
-		//
+		// 1. Select current logged in user ID.  Check ID against ID to edit.
+
+		// 2. if(ID_matches) return form for editing profile
+
+		return View::make('admin.edit_profile');
+
+		// 2. else reroute user to dashboard
 	}
 
 
@@ -67,7 +85,7 @@ class UserController extends \BaseController {
 	 */
 	public function update($id)
 	{
-		//
+		// Update user info!
 	}
 
 
@@ -79,7 +97,7 @@ class UserController extends \BaseController {
 	 */
 	public function destroy($id)
 	{
-		//
+		// Delete user!
 	}
 
 
